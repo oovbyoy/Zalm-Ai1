f67edac15d2303fc5c8f84e49b8658dd761bea51
oovbyoy <saraalprivet@gmail.com>
2025-11-06 12:43:03 +0300

    Add files via upload

---
+++ zalm-vercel/pages/team.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import '../styles/globals.css'
+
+export default function Team(){ 
+  return (
+    <>
+      <Head><title>Zalm AI - Team</title></Head>
+      <div className="centerPage">
+        <div className="card">
+          <h1>Our Team</h1>
+          <ul>
+            <li><b>Mansour</b> - Student</li>
+            <li><b>Abdulaziz Al-Maliki</b> - Teacher</li>
+            <li><b>Eid Nawaf</b> - Developer</li>
+          </ul>
+          <Link href="/"><a className="link">← Back to Login</a></Link>
+        </div>
+      </div>
+    </>
+  );
+}
